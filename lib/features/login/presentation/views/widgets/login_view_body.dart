ef4077c9897b0084c7fb2e58cf07a95bdaaf4d42
Nadine <naloey2015@gmail.com>
2024-04-02 03:24:26 +0200

    login commited

--- lib/features/login/presentation/views/widgets/login_view_body.dart
+++ lib/features/login/presentation/views/widgets/login_view_body.dart
@@ -1,12 +1,85 @@
+import 'package:fixer/features/login/presentation/views/widgets/login_view_body_buttons.dart';
+import 'package:fixer/features/login/presentation/views/widgets/login_view_body_forgotpass_row.dart';
+import 'package:fixer/features/login/presentation/views/widgets/login_view_body_logo.dart';
+import 'package:fixer/features/login/presentation/views/widgets/login_view_body_text_forms';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginViewBody extends StatelessWidget {
-  const LoginViewBody({super.key});
+  LoginViewBody({super.key});
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-        // TODO: implement Login body UI 
-      );
+    return ListView(children: [
+      Column(children: [
+        const Logo(),
+        const SizedBox(height: 25),
+        Container(
+          margin: const EdgeInsets.only(left: 35),
+          alignment: Alignment.topLeft,
+          child: Text("Email",
+              textAlign: TextAlign.start,
+              style: GoogleFonts.roboto(
+                color: const Color(0xff1E1E1E),
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              )),
+        ),
+        const SizedBox(height: 10),
+        //EMAIL INPUT
+        TextForm(
+            controller: emailController,
+            text: "   Enter your email",
+            obscure: false,
+            textInputType: TextInputType.emailAddress),
+        const SizedBox(height: 20),
+        Container(
+          margin: const EdgeInsets.only(left: 35),
+          alignment: Alignment.topLeft,
+          child: Text("Password",
+              textAlign: TextAlign.start,
+              style: GoogleFonts.roboto(
+                color: const Color(0xff1E1E1E),
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              )),
+        ),
+        const SizedBox(height: 10),
+        //PASSWORD INPUT
+        TextForm(
+            controller: passwordController,
+            text: "   Enter your password",
+            textInputType: TextInputType.text,
+            obscure: true),
+        const SizedBox(height: 15),
+        const ForgotPassword(),
+        const SizedBox(height: 20),
+        const ButtonLogin(),
+        const SizedBox(height: 15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Don’t have an account?",
+              style: GoogleFonts.roboto(
+                  color: const Color(0xff000000),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500),
+            ),
+            TextButton(
+                onPressed: () {},
+                child: Text("Sign up",
+                    style: GoogleFonts.roboto(
+                      color: const Color(0XFF303564),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    )))
+          ],
+        )
+      ]),
+    ]);
   }
 }
---
+++ lib/features/login/presentation/views/widgets/login_view_body_logo.dart
@@ -0,0 +1,32 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Stack(children: [
+        Image.asset(
+          "assets/images/Group 22.png",
+          width: 125,
+          height: 89,
+        ),
+        Padding(
+            padding: const EdgeInsets.only(top: 25),
+            child: Image.asset("assets/images/fixr logo.png",
+                width: 134.85, height: 56.92))
+      ]),
+      const SizedBox(height: 40),
+      Text("Welcome Back!",
+          textAlign: TextAlign.center,
+          style: GoogleFonts.roboto(
+            color: const Color(0xff1E1E1E),
+            fontSize: 24,
+            fontWeight: FontWeight.w700,
+          ))
+    ]);
+  }
+}
