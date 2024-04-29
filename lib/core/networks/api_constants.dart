d362ec08e9458d5b6999256011432f0979071667
MagdKamaldev <magddevf@gmail.com>
2024-04-29 11:44:07 +0300

    api services

---
+++ lib/core/networks/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'http://127.0.0.1:5000/';
+  static const String login = 'login';
+  static const String registerClient = 'ClientRegistration';
+  static const String registerAdmin = 'AdminRegistration';
+  static const String getAllAdmins = 'retrive';
+  }
