ad18281c13f6036641cce238a0c1acc84bd5b0f6
Nadine-ali <nadinealiselim@gmail.com>
2024-06-14 00:55:21 +0300

    remove unsed imports

--- lib/features/home/presentation/profile_view/presentation/views/widgets/profile_info.dart
+++ lib/features/home/presentation/profile_view/presentation/views/widgets/profile_info.dart
@@ -1,5 +1,4 @@
 import 'package:fixer/core/themes/colors.dart';
-import 'package:fixer/core/themes/text_styles.dart';
 import 'package:fixer/features/home/presentation/profile_view/presentation/views/widgets/info_row_model.dart';
 import 'package:fixer/generated/l10n.dart';
 import 'package:flutter/material.dart';
